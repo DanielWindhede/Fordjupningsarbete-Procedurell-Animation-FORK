9209920533a2ab3c82d2141a8e6eaca7ca264340
Filip Petersson <zmarfan@hotmail.com>
2020-11-10 18:13:31 +0100

    Added test of ground detection

--- Procedurell Animation Spoider/Assets/Scripts/LegTarget.cs
+++ Procedurell Animation Spoider/Assets/Scripts/LegTarget.cs
@@ -4,5 +4,29 @@ using UnityEngine;
 
 public class LegTarget : MonoBehaviour
 {
+    [SerializeField] Transform _body;
+    [SerializeField] LayerMask _layerMask;
+    [SerializeField] Color _rayCastColor;
+    [SerializeField, Range(0.01f, 100)] float _maxDistance = 1.5f;
+    [SerializeField, Range(-20f, 20)] float _shootRayYPosition = -0.15f;
+    [SerializeField, Range(-20f, 20f)] float _idealYPosition;
+
     public Vector3 Position { get { return this.transform.position; } set { this.transform.position = value; } }
+
+    private void Update()
+    {
+        UpdatePosition();
+    }
+
+    void UpdatePosition()
+    {
+        Vector3 origin = transform.position;
+        origin += _body.up * _shootRayYPosition;
+
+        Debug.DrawLine(origin, origin - _body.up * _maxDistance, _rayCastColor);
+
+        RaycastHit hit;
+        if(Physics.Raycast(origin, -_body.up, out hit, _maxDistance, _layerMask))
+            transform.position = hit.point;
+    }
 }
