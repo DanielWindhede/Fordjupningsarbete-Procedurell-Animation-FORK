1b63354ebf0732d63c4dee5cf976b5a94fa9466f
Filip Petersson <zmarfan@hotmail.com>
2020-11-10 17:06:02 +0100

    Added movement

---
+++ Procedurell Animation Spoider/Assets/ControlSpider.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ControlSpider : MonoBehaviour
+{
+    [Header("Settings")]
+
+    [SerializeField, Range(0.01f, 500f)] float _moveSpeed;
+    [SerializeField, Range(0.01f, 500f)] float _rotateSpeed;
+
+    [Header("Drop")]
+
+    [SerializeField] Transform _body;
+
+    private void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis(InputManager.HORIZONTAL);
+        float vertical = Input.GetAxis(InputManager.VERTICAL);
+        float rotate = Input.GetAxis(InputManager.ROTATE);
+
+        Move(horizontal, vertical);
+        Rotate(rotate);
+    }
+
+    void Move(float horizontal, float vertical)
+    {
+        Vector3 forward = _body.forward * -vertical;
+        Vector3 right = _body.right * -horizontal;
+        Vector3 direction = forward + right;
+        _body.localPosition = _body.localPosition + direction * Time.fixedDeltaTime * _moveSpeed;
+    }
+
+    void Rotate(float rotate)
+    {
+        Vector3 eulerRotation = _body.localRotation.eulerAngles;
+        eulerRotation.y += rotate * _rotateSpeed;
+        _body.localRotation = Quaternion.Euler(eulerRotation);
+    }
+}
--- Procedurell Animation Spoider/Assets/Scripts/LegTarget.cs
+++ Procedurell Animation Spoider/Assets/Scripts/LegTarget.cs
@@ -4,8 +4,5 @@ using UnityEngine;
 
 public class LegTarget : MonoBehaviour
 {
-    [SerializeField] int _index;
-
     public Vector3 Position { get { return this.transform.position; } set { this.transform.position = value; } }
-    public int Index { get { return _index; } }
 }
